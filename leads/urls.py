68925062cb19f516a6673158e900e7e2db2b9e08
Kirill Andreev <hizenberg228@mail.ru>
2021-06-22 21:52:53 +0300

    Add form for creating leads

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import lead_list
+from .views import lead_list, lead_detail, lead_create
 
+app_name = 'leads'
 
 urlpatterns = [
     path('', lead_list),
+    path('<int:pk>/', lead_detail),
+    path('create/', lead_create),
 
 ]
