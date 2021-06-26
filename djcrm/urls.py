57f2578956ad8883932cf5afcf87dbe5030fecc4
Kirill Andreev <hizenberg228@mail.ru>
2021-06-26 16:51:19 +0300

    Added static files

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView
@@ -7,6 +9,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace="leads")), # namespace потом необходимо указывать привызове всех его урлов leads:...
-
-
 ]
+
+# Когда не в прод
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+
